6b928abc6e235a85771d76482cb43a066de48569
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-01-28 14:45:28 +0100

    Update artnet.py to use client objects instead of client IDs.

--- artnet.py
+++ artnet.py
@@ -4,21 +4,22 @@ import gloabals
 def send_command_a():
     print("statup")
     while True:
-        client_id= gloabals.clients[int(input("Enter client ID to sent artnet: "))].client_id
-        dimmer = input("Enter velue dimmer run for client {}: ".format(client_id))
-        hueshift = input("Enter velue hueshift run for client {}: ".format(client_id))
-        animation = input("Enter velue animation run for client {}: ".format(client_id))
-        fx1 = input("Enter velue fx1 run for client {}: ".format(client_id))
-        fx2 = input("Enter velue fx2 run for client {}: ".format(client_id))
-        fx3 = input("Enter velue fx3 run for client {}: ".format(client_id))
-        fx4 = input("Enter velue fx4 run for client {}: ".format(client_id))
-        webserver.socketio.emit('update_artnet',{
-        'dimmer': dimmer,
-        'hueshift': hueshift,
-        'animation': animation,
-        'fx1': fx1,
-        'fx2': fx2,
-        'fx3': fx1,
-        'fx4': fx2
-    }, room=client_id,namespace="/client")
+        client= gloabals.clients[int(input("Enter client ID to sent artnet: "))]
+        dimmer = input(f"Enter velue dimmer run for client {client.name}({client.id}): ")
+        hueshift = input(f"Enter velue hueshift run for client {client.name}({client.id}): ")
+        animation = input(f"Enter velue animation run for client {client.name}({client.id}): ")
+        fx1 = input(f"Enter velue fx1 run for client {client.name}({client.id}): ")
+        fx2 = input(f"Enter velue fx2 run for client {client.name}({client.id}): ")
+        fx3 = input(f"Enter velue fx3 run for client {client.name}({client.id}): ")
+        fx4 = input(f"Enter velue fx4 run for client {client.name}({client.id}): ")
+        data = [0 for i in range(512)]
+        data[client.addresse]=dimmer
+        data[client.addresse+1]=hueshift
+        data[client.addresse+2]=animation
+        data[client.addresse+3]=fx1
+        data[client.addresse+4]=fx2
+        data[client.addresse+5]=fx3
+        data[client.addresse+6]=fx4
+        client.update_artnet(data)
+        
     
