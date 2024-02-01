2cfdf386eeb5de3e458e03654621f5a2726e1b4b
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-02-01 13:38:33 +0100

    continue on wong debug input

--- artnet.py
+++ artnet.py
@@ -4,22 +4,25 @@ import gloabals
 def send_command_a():
     print("statup")
     while True:
-        client= gloabals.clients[int(input("Enter client ID to sent artnet: "))]
-        dimmer = input(f"Enter velue dimmer run for client {client.name}({client.id}): ")
-        hueshift = input(f"Enter velue hueshift run for client {client.name}({client.id}): ")
-        animation = input(f"Enter velue animation run for client {client.name}({client.id}): ")
-        fx1 = input(f"Enter velue fx1 run for client {client.name}({client.id}): ")
-        fx2 = input(f"Enter velue fx2 run for client {client.name}({client.id}): ")
-        fx3 = input(f"Enter velue fx3 run for client {client.name}({client.id}): ")
-        fx4 = input(f"Enter velue fx4 run for client {client.name}({client.id}): ")
-        data = [0 for i in range(512)]
-        data[client.addresse]=dimmer
-        data[client.addresse+1]=hueshift
-        data[client.addresse+2]=animation
-        data[client.addresse+3]=fx1
-        data[client.addresse+4]=fx2
-        data[client.addresse+5]=fx3
-        data[client.addresse+6]=fx4
-        client.update_artnet(data)
+        try:
+            client= gloabals.clients[int(input("Enter client ID to sent artnet: "))]
+            dimmer = input(f"Enter value dimmer for client {client.name}({client.id}): ")
+            hueshift = input(f"Enter value hueshift for client {client.name}({client.id}): ")
+            animation = input(f"Enter value animation for client {client.name}({client.id}): ")
+            fx1 = input(f"Enter value fx1 for client {client.name}({client.id}): ")
+            fx2 = input(f"Enter value fx2 for client {client.name}({client.id}): ")
+            fx3 = input(f"Enter value fx3 for client {client.name}({client.id}): ")
+            fx4 = input(f"Enter value fx4 for client {client.name}({client.id}): ")
+            data = [0 for i in range(512)]
+            data[client.addresse]=dimmer
+            data[client.addresse+1]=hueshift
+            data[client.addresse+2]=animation
+            data[client.addresse+3]=fx1
+            data[client.addresse+4]=fx2
+            data[client.addresse+5]=fx3
+            data[client.addresse+6]=fx4
+            client.update_artnet(data)
+        except:
+            print("An error occurred. Retry from the beginning.")
         
     
