7b8d6cbc3ca28398b3fa3f1d56a8189959cd85fc
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2025-02-20 22:34:57 +0100

    added webpage

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	httpHandler "technikflg.com/dmxtobeamerserver/web"
 )
@@ -31,8 +32,12 @@ func main() {
 	r.HandleFunc("/ws", httpHandler.HandleWS)
 
 	r.PathPrefix("/").HandlerFunc(httpHandler.StaticFileHandler)
-
+	corsHandler := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}), // Allow all origins
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
 	// Start server
 	log.Println("Server started at http://127.0.0.1:8080")
-	http.ListenAndServe(":8080", r)
+	http.ListenAndServe(":8080", corsHandler(r))
 }
